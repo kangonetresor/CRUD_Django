4e96618335dfa7f89598ca8e3a03330f90d25906
kangonetresor <kangonebi@gmail.com>
2024-01-17 00:13:09 +0000

    Mise en place de la première couche de notre interface

--- MonPremierProjetPy_Django/MonPremierProjetPy_Django/urls.py
+++ MonPremierProjetPy_Django/MonPremierProjetPy_Django/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from GestionetudiantCRUD import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.add_show, name = "addandshow"),
 ]
